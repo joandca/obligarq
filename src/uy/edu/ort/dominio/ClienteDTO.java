f0aba2573e7402b5013764418ca62034b47faaf8
joandca <joandca@gmail.com>
2015-06-18 23:32:33 -0300

    Primer Commit de dominio

---
+++ src/uy/edu/ort/dominio/ClienteDTO.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uy.edu.ort.dominio;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ClienteDTO implements Serializable {
+
+    private String nombre;
+
+    private String apellido;
+
+    private String ci;
+
+    private String direccion;
+
+    private String telefono;
+    
+    public ClienteDTO() {}
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getCi() {
+        return ci;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setCi(String ci) {
+        this.ci = ci;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
